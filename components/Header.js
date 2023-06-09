424484992ed881b452b8a63fc0810b777e06a94e
suthee <lovalek@gmail.com>
2023-06-09 09:44:40 +0700

    feat(homepage): add header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,22 @@
+
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="bg-gray-500">
-            <Link href={'/'} >E-commerce</Link>
-            <nav>
+        <header className="bg-gray-700 text-white flex justify-between px-[5%] pt-4">
+            <Link 
+                className="flex"
+                href={'/'}>
+                    <Image 
+                    className="-mt-5"
+                    alt="Landscape picture"
+                    width={70}
+                    height={60}
+                    src='/images/logo/LogoU.png'/> 
+                    STOCK FOOTO
+            </Link>
+            <nav className="flex gap-4">
                 <Link href={'/'}>Home</Link>
                 <Link href={'/products'}>All products</Link>
                 <Link href={'/categories'}>Categories</Link>
